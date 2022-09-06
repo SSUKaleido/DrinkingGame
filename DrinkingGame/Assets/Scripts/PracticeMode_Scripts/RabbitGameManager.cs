d6303397ba1260df1ec8847562349b2443590dbb
Jongeun <105369412+Lee-jongeun@users.noreply.github.com>
2022-09-06 19:29:26 +0900

    Develop UI

--- DrinkingGame/Assets/Scripts/PracticeMode_Scripts/RabbitGameManager.cs
+++ DrinkingGame/Assets/Scripts/PracticeMode_Scripts/RabbitGameManager.cs
@@ -2,34 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class RabbitGameManager : MonoBehaviour
 {
-    ImageLineRenderer ArrowLine;
+    public ImageLineRenderer ArrowLine;
+    public RectTransform[] Person;
 
-    Vector3 PlayerPoint = new Vector3(960, 340, 0);
-    Vector3 Person1Point = new Vector3(1101.4214f, 399.4214f, 0);
-    Vector3 Person2Point = new Vector3(1160, 540, 0);
-    Vector3 Person3Point = new Vector3(1101.4214f, 681.4214f, 0);
-    Vector3 Person4Point = new Vector3(960, 740, 0);
-    Vector3 Person5Point = new Vector3(818.4214f, 681.4214f, 0);
-    Vector3 Person6Point = new Vector3(760, 540, 0);
-    Vector3 Person7Point = new Vector3(818.4214f, 399.4214f, 0);
+    Vector3[] PersonPoint = new Vector3[8];
 
-    void Awake() {
-        ArrowLine = GameObject.Find("ArrowLine").GetComponent<ImageLineRenderer>();
-    }
+    bool IsGameStart = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        ArrowLine.PointA = Person3Point;
-        ArrowLine.PointB = PlayerPoint;
+        PersonPoint[0] = new Vector3(960, 340, 0);
+        PersonPoint[1] = new Vector3(1101.4214f, 399.4214f, 0);
+        PersonPoint[2] = new Vector3(1160, 540, 0);
+        PersonPoint[3] = new Vector3(1101.4214f, 681.4214f, 0);
+        PersonPoint[4] = new Vector3(960, 740, 0);
+        PersonPoint[5] = new Vector3(818.4214f, 681.4214f, 0);
+        PersonPoint[6] = new Vector3(760, 540, 0);
+        PersonPoint[7] = new Vector3(818.4214f, 399.4214f, 0);
+
+        ArrowLine.PointA = PersonPoint[3];
+        ArrowLine.PointB = PersonPoint[0];
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Q)) {
+            HorizontalShakePerson(Person[1]);
+       }
+        if (Input.GetKeyDown(KeyCode.W)) {
+            VerticalShakePerson(Person[1]);
+        }
+    }
 
+    public void GameStart() {
+        IsGameStart = true;
+    }
+
+    void HorizontalShakePerson(RectTransform Person) {
+        Person.DORotate(new Vector3(0.0f, 0.0f, 20.0f), 0.3f);
+        Person.DORotate(new Vector3(0.0f, 0.0f, -20.0f), 0.6f).SetDelay(0.3f);
+        Person.DORotate(new Vector3(0.0f, 0.0f, 0.0f), 0.3f).SetDelay(0.7f);
+    }
+
+    void VerticalShakePerson(RectTransform Person) {
+        Person.DOJump(Person.transform.position, 25.0f, 1, 0.5f, true);
+        Person.DOJump(Person.transform.position, 25.0f, 1, 0.5f, true).SetDelay(0.9f);
     }
 }
