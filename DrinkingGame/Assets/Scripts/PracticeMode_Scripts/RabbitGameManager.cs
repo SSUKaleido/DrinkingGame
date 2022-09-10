6d0d9eedd4475c4e601e81bc46ab85eb0fd3cf3a
Jongeun <105369412+Lee-jongeun@users.noreply.github.com>
2022-09-10 18:47:40 +0900

    Develop Rabbit Game

--- DrinkingGame/Assets/Scripts/PracticeMode_Scripts/RabbitGameManager.cs
+++ DrinkingGame/Assets/Scripts/PracticeMode_Scripts/RabbitGameManager.cs
@@ -8,14 +8,16 @@ public class RabbitGameManager : MonoBehaviour
 {
     public ImageLineRenderer ArrowLine;
     public Image RecurrsiveArrowLine;
+    public Image[] ExclamationMark;
     public RectTransform[] Person;
     public Slider TimerSlider;
 
     Vector3[] PersonPoint = new Vector3[8];
+    Vector3 MousePosition;
 
-    int TaggerNum = 0, RandomNum;
-    float time = 0.0f , TimerUpper = 0.5f;
-    bool IsGameStart = false, IsPlayerLeftMove = true, IsPlayerRightMove = true;
+    int RandomNum = 0, BeforeNum = 0, TurnNum = 0;
+    float time = 0.0f, TimerUpper = 0.5f;
+    bool IsGameStart = false, IsPlayerLeftMove = true, IsPlayerRightMove = true, IsPlayerChoiceTurn = false;
 
     // Start is called before the first frame update
     void Start()
@@ -29,36 +31,136 @@ public class RabbitGameManager : MonoBehaviour
         PersonPoint[6] = new Vector3(836.8629f, 643.1371f, 0);
         PersonPoint[7] = new Vector3(800, 540, 0);
 
+        ExclamationMark[0].enabled = false;
+        ExclamationMark[1].enabled = false;
+        ExclamationMark[2].enabled = false;
+        ExclamationMark[3].enabled = false;
+        ExclamationMark[4].enabled = false;
+        ExclamationMark[5].enabled = false;
+        ExclamationMark[6].enabled = false;
+        ExclamationMark[7].enabled = false;
+
         RecurrsiveArrowLine.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (IsGameStart == true) {
+        if (IsGameStart == true && IsPlayerChoiceTurn == false) {
             time += Time.deltaTime;
-            if (time > TimerUpper)
-                NextTurn_Computer();
+            if (time > TimerUpper) {
+                if(IsPlayerLeftMove == false || IsPlayerRightMove == false) {
+                    GameOver(0);
+                }
+
+                time = 0.0f;
+                TurnNum++;
+                if (ArrowLine.PointB != PersonPoint[0])
+                    NextTurn_Computer();
+                else
+                    ChoiceTurn_Player();
+
+                if (TimerUpper == 0.5f) {
+                    TimerUpper = 2.0f;
+                    if (TurnNum > 20 && Random.Range(0.0f, 100.0f) < TurnNum * 0.1f)
+                        GameWin();
+                    else if (RandomNum != 0)
+                        Shake_ComputerPerson();
+                }
+                else if (TimerUpper == 2.0f)
+                    TimerUpper = 0.5f;
+            }
 
             TimerSlider.value = (time / TimerUpper);
 
-            if (IsPlayerLeftMove == false && Input.GetKeyDown(KeyCode.LeftArrow))
-                IsPlayerLeftMove = LeftShake_PlayerPerson();
-            if (IsPlayerRightMove == false && Input.GetKeyDown(KeyCode.RightArrow))
-                IsPlayerRightMove = RightShake_PlayerPerson();
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+                LeftShake_PlayerPerson();
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+                RightShake_PlayerPerson();
+        }
+        
+        else if (IsGameStart == true && IsPlayerChoiceTurn == true) {
+            MousePosition = Input.mousePosition;
+            ArrowLine.PointB = MousePosition;
+
         }
     }
 
     public void GameStart() {
         time = 0.0f;
-        ArrowLine.PointA = PersonPoint[0];
-        ArrowLine.PointB = PersonPoint[0];
+        TimerUpper = 2.0f;
+        TurnNum = 0;
+        RandomNum = Random.Range(1, 8);
+        BeforeNum = 5;
+        ArrowLine.PointA = PersonPoint[5];
+        ArrowLine.PointB = PersonPoint[RandomNum];
         IsGameStart = true;
+        IsPlayerLeftMove = true;
+        IsPlayerRightMove = true;
+
+        ExclamationMark[0].enabled = false;
+        ExclamationMark[1].enabled = false;
+        ExclamationMark[2].enabled = false;
+        ExclamationMark[3].enabled = false;
+        ExclamationMark[4].enabled = false;
+        ExclamationMark[5].enabled = false;
+        ExclamationMark[6].enabled = false;
+        ExclamationMark[7].enabled = false;
+
+        Shake_ComputerPerson();
+    }
+
+    void GameOver(int taggerNum) {
+        IsGameStart = false;
+        ExclamationMark[taggerNum].enabled = true;
+    }
+
+    void GameWin() {
+        RandomNum = Random.Range(0, 8);
+        ArrowLine.PointA = ArrowLine.PointB;
+        ArrowLine.PointB = PersonPoint[RandomNum];
+
+        int TempRan = Random.Range(1, 2);
+        if (RandomNum >= 4 && RandomNum <= 6 && TempRan == 1) {
+            GameOver(RandomNum - 1);
+            HorizontalShake_CompterPerson(Person[RandomNum + 1]);
+        }
+        else if (RandomNum >= 4 && RandomNum <= 6 && TempRan == 2) {
+            HorizontalShake_CompterPerson(Person[RandomNum - 1]);
+            GameOver(RandomNum + 1);
+        }
+        else if (RandomNum == 7 && TempRan == 1) {
+            GameOver(6);
+            HorizontalShake_CompterPerson(Person[2]);
+        }
+        else if (RandomNum == 7 && TempRan == 2) {
+            HorizontalShake_CompterPerson(Person[6]);
+            GameOver(2);
+        }
+        else if (RandomNum == 3 && TempRan == 1) {
+            GameOver(1);
+            HorizontalShake_CompterPerson(Person[4]);
+        }
+        else if (RandomNum == 3 && TempRan == 2) {
+            HorizontalShake_CompterPerson(Person[1]);
+            GameOver(4);
+        }
+        else if (RandomNum == 1 || RandomNum == 2) {
+            GameOver((RandomNum == 1) ? 3 : 7);
+        }
+        else if (RandomNum == 0 && TempRan == 1) {
+            GameOver(1);
+            HorizontalShake_CompterPerson(Person[2]);
+        }
+        else if (RandomNum == 0 && TempRan == 2) {
+            HorizontalShake_CompterPerson(Person[1]);
+            GameOver(2);
+        }
     }
 
     void NextTurn_Computer() {
-        time = 0.0f;
         if (TimerUpper == 2.0f) {
+            BeforeNum = RandomNum;
             RandomNum = Random.Range(1, 21);
             if (RandomNum <= 15)
                 RandomNum = (RandomNum - 1) / 5;
@@ -74,45 +176,75 @@ public class RabbitGameManager : MonoBehaviour
             RecurrsiveArrowLine.enabled = true;
             RecurrsiveArrowLine.transform.position = ArrowLine.PointA;
         }
+    }
 
-        if ((RandomNum >= 4 && RandomNum <= 6) && TimerUpper == 0.5f) {
+    void ChoiceTurn_Player() {
+        ArrowLine.PointA = ArrowLine.PointB;
+        VerticalShakePerson(Person[BeforeNum]);
+        IsPlayerChoiceTurn = true;
+    }
+
+    public void MouseClick_Character(int index) {
+        if (IsPlayerChoiceTurn == true) {
+            RandomNum = index;
+            BeforeNum = 0;
+            IsPlayerChoiceTurn = false;
+            IsPlayerLeftMove = true;
+            IsPlayerRightMove = true;
+            ArrowLine.PointA = PersonPoint[0];
+            ArrowLine.PointB = PersonPoint[index];
+            Shake_ComputerPerson();
+            TimerUpper = 2.0f;
+            time = 0.0f;
+        }
+    }
+
+    void Shake_ComputerPerson() {
+        VerticalShakePerson(Person[BeforeNum]);
+
+        if (RandomNum >= 4 && RandomNum <= 6) {
             HorizontalShake_CompterPerson(Person[RandomNum - 1]);
             HorizontalShake_CompterPerson(Person[RandomNum + 1]);
         }
-        else if (RandomNum == 7 && TimerUpper == 0.5f) {
+        else if (RandomNum == 7) {
             HorizontalShake_CompterPerson(Person[6]);
             HorizontalShake_CompterPerson(Person[2]);
         }
-        else if (RandomNum == 3 && TimerUpper == 0.5f) {
+        else if (RandomNum == 3) {
             HorizontalShake_CompterPerson(Person[1]);
             HorizontalShake_CompterPerson(Person[4]);
         }
-        else if ((RandomNum == 1 || RandomNum == 2) && TimerUpper == 0.5f) {
+        else if (RandomNum == 1 || RandomNum == 2) {
             HorizontalShake_CompterPerson(Person[(RandomNum == 1) ? 3 : 7]);
             IsPlayerLeftMove = false;
             IsPlayerRightMove = false;
         }
-        else if (RandomNum == 0 && TimerUpper == 0.5f) {
+        else if (RandomNum == 0) {
             HorizontalShake_CompterPerson(Person[1]);
             HorizontalShake_CompterPerson(Person[2]);
         }
-
-        if (TimerUpper == 0.5f)
-            TimerUpper = 2.0f;
-        else if (TimerUpper == 2.0f)
-            TimerUpper = 0.5f;
     }
 
-    bool LeftShake_PlayerPerson() {
-        Person[0].DORotate(new Vector3(0.0f, 0.0f, 20.0f), 0.3f);
-        Person[0].DORotate(new Vector3(0.0f, 0.0f, 0.0f), 0.3f).SetDelay(0.3f);
-        return true;
+    void LeftShake_PlayerPerson() {
+        if (IsPlayerLeftMove == false) {
+            Person[0].DORotate(new Vector3(0.0f, 0.0f, 20.0f), 0.3f);
+            Person[0].DORotate(new Vector3(0.0f, 0.0f, 0.0f), 0.3f).SetDelay(0.3f);
+            IsPlayerLeftMove = true;
+        }
+        else {
+            GameOver(0);
+        }
     }
 
-    bool RightShake_PlayerPerson() {
-        Person[0].DORotate(new Vector3(0.0f, 0.0f, -20.0f), 0.3f);
-        Person[0].DORotate(new Vector3(0.0f, 0.0f, 0.0f), 0.3f).SetDelay(0.3f);
-        return true;
+    void RightShake_PlayerPerson() {
+        if (IsPlayerRightMove == false) {
+            Person[0].DORotate(new Vector3(0.0f, 0.0f, -20.0f), 0.3f);
+            Person[0].DORotate(new Vector3(0.0f, 0.0f, 0.0f), 0.3f).SetDelay(0.3f);
+            IsPlayerRightMove = true;
+        }
+        else {
+            GameOver(0);
+        }
     }
 
     void HorizontalShake_CompterPerson(RectTransform Person) {
