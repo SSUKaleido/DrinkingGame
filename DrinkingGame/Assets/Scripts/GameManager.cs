68bacf48b82b986df691b7a3f92578445d999897
Jongeun <105369412+Lee-jongeun@users.noreply.github.com>
2022-09-12 08:00:50 +0900

    Fix GameManager and Designate Pivot

--- DrinkingGame/Assets/Scripts/GameManager.cs
+++ DrinkingGame/Assets/Scripts/GameManager.cs
@@ -8,14 +8,14 @@ public class GameManager : MonoBehaviour
 {
     public Canvas StartMenu;
     public Canvas PracticeMode_SelectMenu;
-    public Canvas PracticeMode_TrainGame;
+    public Canvas PracticeMode_HangeulGame;
     public Canvas PracticeMode_BaskinGame;
     public Canvas PracticeMode_RabbitGame;
     public Canvas PracticeMode_FruitsGame;
 
     public CanvasGroup StartMenu_Group;
     public CanvasGroup PracticeMode_SelectMenu_Group;
-    public CanvasGroup PracticeMode_TrainGame_Group;
+    public CanvasGroup PracticeMode_HangeulGame_Group;
     public CanvasGroup PracticeMode_BaskinGame_Group;
     public CanvasGroup PracticeMode_RabbitGame_Group;
     public CanvasGroup PracticeMode_FruitsGame_Group;
@@ -27,6 +27,15 @@ public class GameManager : MonoBehaviour
     {
         StartMenu.enabled = true;
         PracticeMode_SelectMenu.enabled = false;
+        PracticeMode_SelectMenu_Group.interactable = false;
+        PracticeMode_HangeulGame.enabled = false;
+        PracticeMode_HangeulGame_Group.interactable = false;
+        PracticeMode_BaskinGame.enabled = false;
+        PracticeMode_BaskinGame_Group.interactable = false;
+        PracticeMode_RabbitGame.enabled = false;
+        PracticeMode_RabbitGame_Group.interactable = false;
+        PracticeMode_FruitsGame.enabled = false;
+        PracticeMode_FruitsGame_Group.interactable = false;
     }
 
     // Update is called once per frame
@@ -57,28 +66,60 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void LoadPracticeGame_Train() {
-        PracticeMode_TrainGame.enabled = true;
-        PracticeMode_TrainGame_Group.interactable = true;
+    public void LoadPracticeGame_Hangeul() {
+        PracticeMode_HangeulGame.enabled = true;
+        PracticeMode_HangeulGame_Group.interactable = true;
         PracticeMode_SelectMenu.enabled = false;
+        PracticeMode_SelectMenu_Group.interactable = false;
     }
 
     public void LoadPracticeGame_Baskin() {
         PracticeMode_BaskinGame.enabled = true;
         PracticeMode_BaskinGame_Group.interactable = true;
         PracticeMode_SelectMenu.enabled = false;
+        PracticeMode_SelectMenu_Group.interactable = false;
     }
 
     public void LoadPracticeGame_Rabbit() {
         PracticeMode_RabbitGame.enabled = true;
         PracticeMode_RabbitGame_Group.interactable = true;
         PracticeMode_SelectMenu.enabled = false;
+        PracticeMode_SelectMenu_Group.interactable = false;
     }
 
     public void LoadPracticeGame_Fruits() {
         PracticeMode_FruitsGame.enabled = true;
         PracticeMode_FruitsGame_Group.interactable = true;
         PracticeMode_SelectMenu.enabled = false;
+        PracticeMode_SelectMenu_Group.interactable = false;
+    }
+
+    public void UnloadPracticeGame_Hangeul() {
+        PracticeMode_SelectMenu.enabled = true;
+        PracticeMode_SelectMenu_Group.interactable = true;
+        PracticeMode_HangeulGame.enabled = false;
+        PracticeMode_HangeulGame_Group.interactable = false;
+    }
+
+    public void UnloadPracticeGame_Baskin() {
+        PracticeMode_SelectMenu.enabled = true;
+        PracticeMode_SelectMenu_Group.interactable = true;
+        PracticeMode_BaskinGame.enabled = false;
+        PracticeMode_BaskinGame_Group.interactable = false;
+    }
+
+    public void UnloadPracticeGame_Rabbit() {
+        PracticeMode_SelectMenu.enabled = true;
+        PracticeMode_SelectMenu_Group.interactable = true;
+        PracticeMode_RabbitGame.enabled = false;
+        PracticeMode_RabbitGame_Group.interactable = false;
+    }
+
+    public void UnloadPracticeGame_Fruits() {
+        PracticeMode_SelectMenu.enabled = true;
+        PracticeMode_SelectMenu_Group.interactable = true;
+        PracticeMode_FruitsGame.enabled = false;
+        PracticeMode_FruitsGame_Group.interactable = false;
     }
 
     public void Conclusion() {
